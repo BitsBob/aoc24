547b59ef48d177d78bd468de2b54e622a5eb0818
Audey Godfrey <audeygodfrey@gmail.com>
2024-12-01 10:01:57 +0000

    cleanup

---
+++ utils/utils.h
@@ -0,0 +1,23 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <vector>
+
+#define LOG(x) std::cout << "[LOG] " << x << std::endl;
+#define ERROR_LOG(x) std::cerr << "[ERROR] " << x << std::endl;
+
+#define ASSERT(condition, msg) \
+if (!(condition)) { \
+std::cerr << "[ASSERT] " << msg << std::endl; \
+std::abort(); \
+}
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+std::vector<std::string> read_from_file(const std::string &filename, int &num_lines);
+
+std::vector<std::string> split(const std::string& s, const std::string& delimiter);
+
+#endif // UTILS_H
